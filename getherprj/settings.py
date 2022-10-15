d2f0dedad8c35e4bd5463cb45c8bc6818dd5f0b6
Rommmu <tonty1222@naver.com>
2022-10-15 22:57:23 +0900

    af

--- getherprj/settings.py
+++ getherprj/settings.py
@@ -140,6 +140,3 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Heroku
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
